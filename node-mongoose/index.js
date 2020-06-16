686c6a1051cd370e46f26eaf6703a18df1f31dd6
Utkarsh Nigam <prateek.nigam.581@gmail.com>
2020-06-16 23:38:50 +0530

    Mongoose Part-2&3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,35 +1,47 @@
 const mongoose = require('mongoose');
-
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
 
 connect.then((db) => {
 
     console.log('Connected correctly to server');
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set:{description:'Updated test'},
+            
+        },{
+            new: true
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
-    });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'Test Comment',
+            author: 'Leonardo Di Caprio'
         });
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish);
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 
 });
