0c227b0c1d3c026e5d19af39b2652915f8ddf875
Utkarsh Nigam <prateek.nigam.581@gmail.com>
2020-06-18 16:52:49 +0530

    Basic Authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -31,13 +31,49 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
+function auth(req, res, next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  
+  if(!authHeader){
+    var err = new Error("You ain't authorised bitch!");
+    
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password'){
+    next();
+  }
+  else{
+    var err = new Error("Invalid Credentials");
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+}
+
+app.use(auth);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 app.use('/', indexRouter);
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,48 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Promotions = require('../models/promotions');
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('Will send all the promos to you!');
+    Promotions.find({})
+    .then((promos)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promos)
+    })
+    .catch((err)=>next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the promo: ' + req.body.name + ' with details: ' + req.body.description);
+    Promotions.create(req.body)
+    .then((promo)=>{
+        console.log("Promotion Created: ", promo);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(promo);
+    })
+    .catch(err=>next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all promotions');
+    Promotions.deleteMany({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    })
+    .catch(err => next(err));
 });
 
 promoRouter.route('/:promoId')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req,res,next) => {
+    Promotions.findById(req.params.promoId)
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-.get((req, res,next)=>{
-    res.end("Will send details of promo: " + req.params.promoId);
-})
-.post((req,res,next)=>{
+.post((req, res, next) => {
     res.statusCode = 403;
-    res.end('POST operation not supported');
+    res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put((req,res,next)=>{
-    res.end('Will update the promo: ' + req.params.promoId);
+.put((req, res, next) => {
+    Promotions.findByIdAndUpdate(req.params.promoId, {
+        $set: req.body
+    }, { new: true })
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promo);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-.delete((req, res, next)=>{
-    res.end("Deleting promo: " + req.params.promoId);
+.delete((req, res, next) => {
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 module.exports = promoRouter;
